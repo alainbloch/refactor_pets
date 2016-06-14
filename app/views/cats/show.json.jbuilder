cf0d06efa779c29664a80cd198744693b709ba6b
Alain Bloch <alainbloch@gmail.com>
2016-06-14 16:08:18 -0700

    hello world.

---
+++ app/views/cats/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @cat, :id, :name, :description, :owner_id, :created_at, :updated_at
---
+++ db/migrate/20160614224946_create_cats.rb
@@ -0,0 +1,11 @@
+class CreateCats < ActiveRecord::Migration
+  def change
+    create_table :cats do |t|
+      t.string :name
+      t.text :description
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
